24a176cc99c1692031f9db28083eac8b42d7f4e7
parkchaehyun <chhue96@gmail.com>
2023-07-20 13:18:07 +0900

    data binding

--- app/src/main/java/com/chhue/tpboardapp/MainActivity.kt
+++ app/src/main/java/com/chhue/tpboardapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.chhue.tpboardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chhue.tpboardapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
